7ff2d3e6711d5e78a2b6dccb7761e0d49beedd2a
Carolina Urrutia <carolina.urrutia.iturrieta@gmail.com>
2022-11-22 22:37:00 -0300

    cards

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload = function() {
+//   //write your code here
+//   let symbol = ["♣", "♠", "♥", "♦"];
+//   let numbers = ["A","2","3","4","5","6","7","8","9","J","Q","K"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//   let symbolRandom = Math.floor(Math.random() * numbers.length);
+//   let numbersRandom = Math.floor(Math.random() * numero.length);
+//   console.log(numbers[symbol] + " " + numero[nu]);
+
+//   let carta = document.getElementsByClassName("numbers");
+//   carta[0].innerHTML = numbers[symbol];
+//   carta[1].innerHTML = numbers[symbol];
+
+//   document.querySelector(".num").innerHTML = numero[nu];
+// };
+
+// console.log(rellenorandom, simbolorandom);
+//   let Simbolo1 = document.querySelector("#carta1");
+//   Simbolo1.innerHTML = simbolorandom;
+//   let SimboloMedio = document.querySelector("#SimboloMedio");
+//   SimboloMedio.innerHTML = rellenorandom;
+//   let Simbolo11 = document.querySelector("#carta2");
+//   Simbolo11.innerHTML = simbolorandom;
+
+//   if (simbolorandom == "♥" || simbolorandom == "♦") {
+//     document.querySelector("#carta1").style.color = "red";
+//     document.querySelector("#carta2").style.color = "red";
+//   }
+// };
